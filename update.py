507abdf44c3acfa85d777199082657d648fbd153
ratanavv <57977063+ratanavv@users.noreply.github.com>
2023-12-12 00:44:36 +0800

    Update update.py

--- update.py
+++ update.py
@@ -5,10 +5,6 @@ from altparse import AltSourceManager, Parser, altsource_from_file
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-########################
-## ALTSTORE COMPLETE
-########################
-
 sources_data = [
     {
         "parser": Parser.GITHUB,
@@ -20,14 +16,14 @@ alternate_app_data = {
         "none"
 }
 
-src = altsource_from_file("altstore-complete.json")
-alt_complete = AltSourceManager(src, sources_data)
+src = altsource_from_file("veorarepo.json")
+veora_repo = AltSourceManager(src, sources_data)
 try:
-    alt_complete.update()
-    alt_complete.update_hashes()
-    alt_complete.alter_app_info(alternate_app_data)
-    alt_complete.save()
-    alt_complete.save(alternate_dir="dist/altstore-complete.min.json",prettify=False)
+    veora_repo.update()
+    veora_repo.update_hashes()
+    #veora_repo.alter_app_info(alternate_app_data)
+    veora_repo.save()
+    veora_repo.save(alternate_dir="dist/veorarepo.min.json",prettify=False)
 except Exception as err:
     logging.error(f"Unable to update {alt_complete.src.name}.")
     logging.error(f"{type(err).__name__}: {str(err)}")
